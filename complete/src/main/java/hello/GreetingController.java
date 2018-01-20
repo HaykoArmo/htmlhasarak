e692bbb66ad84560fa39397bb93a91415a868b9a
student <dadyan.d@mail.ru>
2018-01-20 21:37:45 +0400

    html2.6

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private UserService userService;
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
@@ -20,8 +23,24 @@ public class GreetingController {
         return "hello";
     }
 
-    @RequestMapping(value = "/register",method = RequestMethod.GET)
-    public String register() {
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String registerGet() {
         return "register";
     }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String registerPost(@RequestParam("first_name") String first_name,
+                               @RequestParam("last_name") String last_name,
+                               @RequestParam("email") String email,
+                               @RequestParam("password") String password,
+                               @RequestParam("password_repeat") String password_repeat) {
+        if (password.equals(password_repeat)) {
+            System.out.println(first_name + " " + last_name + " " + email + " " + password + " " + password_repeat);
+            userService.create(new User(first_name, last_name, email, password));
+            return "hello";
+        } else {
+            throw new IllegalArgumentException("repeat@ che hamapatasxanum passwordin");
+        }
+    }
+
 }
